ba002e6851cf282ec4fc4467c032e8808e8605c2
unknown <you@example.com>
2022-05-24 11:25:50 +0530

    Addition JAva Code

---
+++ Divine Java/Addition.java
@@ -0,0 +1,11 @@
+class Addition
+{
+   public static void main(String[] args)
+{
+    int a=10;
+    int b=10;
+     int c = a+b;
+
+   System.out.println("The addition of two numbers is:"+c);
+}
+}
