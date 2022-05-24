5124c78f227741e41e244e92f6e24dd36773e600
unknown <you@example.com>
2022-05-24 11:30:18 +0530

    student file

---
+++ Divine Java/StudentFile.java
@@ -0,0 +1,13 @@
+class StudentFile
+{
+  public static void  main(String args[])
+{
+  Student studentdetail = new Student();
+  studentdetail.id = 20;
+  studentdetail.name = "ABC";
+  studentdetail.DOB = 16/9/1997;
+  studentdetail.age = 25;
+
+  studentdetail.stud();
+}
+}
