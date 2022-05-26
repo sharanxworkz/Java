7115b8cee41891c1cbc448c99715a745d92b4c8a
SANNIDHI <you@example.com>
2022-05-26 11:07:41 +0530

    bahubali

--- core_java/src/com/xwrokz/singleinheritance/core_java/Bahubali.java
+++ core_java/src/com/xwrokz/singleinheritance/core_java/Bahubali.java
@@ -2,7 +2,7 @@ package com.xwrokz.singleinheritance.core_java;
 
 public class Bahubali extends Movie {
 	void good() {
-		System.out.println("movie is good");
+		System.out.println("movidgdfe is good");
 	}
 
 }
