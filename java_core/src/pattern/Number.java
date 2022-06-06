0232ded5c0dd97171cee4eec1ef26d2e103076ad
SANNIDHI <you@example.com>
2022-06-07 01:42:58 +0530

    number

---
+++ java_core/src/pattern/Number.java
@@ -0,0 +1,17 @@
+package pattern;
+
+public class Number {
+	public static void main(String[] args) {
+		for(int i=0;i<=1;i++) {
+			for (int j=2;j<=8;j++) {
+				if(j%2==0)
+				{
+					System.out.print(j);
+				}
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
